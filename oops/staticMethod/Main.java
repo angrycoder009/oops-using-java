481e48e0222c4f2d8bd44697245268527da758b7
Gopal yadav <yadavgopal6675@gmail.com>
2024-08-13 05:25:29 +0530

    packages and static method

--- oops/staticMethod/Main.java
+++ oops/staticMethod/Main.java
@@ -1,7 +1,28 @@
 package oops.staticMethod;
 
+//import static oops.medium.Message.message;
+
+import oops.medium.Human;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world!!!!");
+//        System.out.println("hello world!!!!");
+//        message();
+
+
+        Human gopal = new Human(21,"Gopal Yadav",10,false);
+        Human rahul = new Human(24,"rahul",50,true);
+        Human arpit = new Human(24,"rahul",50,true);
+        System.out.println(Human.population);
+//        greeting();
     }
+
+     static  void fund(){
+//        greeting();
+     }
+    //we know that something  which is not static , belongs to an object
+    void greeting(){
+        fund();
+       System.out.println("hello world");
+   }
 }
